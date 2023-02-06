bffc4d9e46a14873da1a7468f75ef2f03ef361f1
Nav <navdeep.dhamrait@thebioagency.com>
2023-02-06 10:26:23 -0500

    refactor

--- src/Components/Screens/ProductDetail.jsx
+++ src/Components/Screens/ProductDetail.jsx
@@ -9,11 +9,9 @@ export default function ProductDetail({ route }) {
         if (item === 0) {
             ref.current.scrollTo({ x: 0, y: 0, animated: true });
             setActiveItem(item);
-
         } else if (item === 1) {
             ref.current.scrollTo({ x: 0, y: 500, animated: true });
             setActiveItem(item);
-
         } else if (item === 2) {
             ref.current.scrollTo({ x: 0, y: 1800, animated: true });
             setActiveItem(item);
@@ -24,8 +22,18 @@ export default function ProductDetail({ route }) {
     const { type } = route.params;
     return (
         <View>
-            <TabContent type={type} activeItem={activeItem} onPress={onPressHandler} />
-            <ScrollView showsVerticalScrollIndicator={false} style={styles.scrollView} ref={ref}>
+            <TabContent
+                type={type}
+                activeItem={activeItem}
+                onPress={onPressHandler}
+              
+            />
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                style={styles.scrollView}
+                ref={ref}
+              
+            >
                 <Text style={styles.calorieContent}>
                     Calories do not reflect customization or additional ingredients. Adults and
                     youth (ages 13 and older) need an average of 2,000 calories a day, and children
